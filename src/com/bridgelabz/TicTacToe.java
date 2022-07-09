59ac0c895c87c7d527509be8a1701175d2fc15a7
Ashwini Rathod <rashwini510@gmail.com>
2022-07-09 09:02:28 +0530

    Added: Ability to create tic tac board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,29 @@
 package com.bridgelabz;
 
 public class TicTacToe {
+
+    public static char[] gameArray = new char[10];
+
+    public static void ticTacToeGame() {
+        for (int i = 0; i < gameArray.length; i++) {
+            gameArray[i] = ' ';
+        }
+
+    }
+
+    // method to show board
+    public static void showBoard() {
+        System.out.println(gameArray[0] + "|" + gameArray[1] + "|" + gameArray[2]);
+        System.out.println(gameArray[3] + "|" + gameArray[4] + "|" + gameArray[5]);
+        System.out.println(gameArray[6] + "|" + gameArray[7] + "|" + gameArray[8]);
+    }
+
+
+    // main method
     public static void main(String[] args) {
-        System.out.println("Welcome to tic tac toe program");
+        System.out.println("Welcome to tic toe program");
+        System.out.println();
+        ticTacToeGame();
+        showBoard();
     }
 }
