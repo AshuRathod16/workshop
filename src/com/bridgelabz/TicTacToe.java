814b9b0a6898db2acf3ab12058616f57edf3a79a
Ashwini Rathod <rashwini510@gmail.com>
2022-07-09 09:28:46 +0530

    Added: Ability to show current board for choosing valid cells

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class TicTacToe {
 
     public static char[] gameArray = new char[10];
-    public static char playerTurn;
-    public static char computerTurn;
-    public static char user = '1';
+
     public static Scanner scanner = new Scanner(System.in);
 
     // main method
     public static void main(String[] args) {
         System.out.println("Welcome to tic tac program");
+        ticTacToeGame();
         showBoard();
         userChoice();
 
     }
 
     // method to show board
-     public static void showBoard() {
+    public static void showBoard() {
         System.out.println(gameArray[0] + "|" + gameArray[1] + "|" + gameArray[2]);
         System.out.println(gameArray[3] + "|" + gameArray[4] + "|" + gameArray[5]);
         System.out.println(gameArray[6] + "|" + gameArray[7] + "|" + gameArray[8]);
@@ -29,6 +28,13 @@ public class TicTacToe {
     public static char userChoice() {
         System.out.println("Enter your choice -> (X or 0) only");
         return scanner.next().toUpperCase().charAt(0);
+    }
+
+    public static void ticTacToeGame() {
+
+        for (int i = 0; i < gameArray.length; i++) {
+            gameArray[i] = ' ';
+        }
     }
 
 }
